ba04ff17473813e15cda2ecf2387090b177166f8
Derick Jin <jinderick5@gmail.com>
2022-10-10 12:27:39 -0700

    1

--- q1.cpp
+++ q1.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 //********************
 // Declare Function Prototypes
+int getinput(int n1, int n2);
+int swapTwoValues(float n1, float n2);
 //********************
 
 int main()
 {
 	int num1, num2;
 	float fnum1, fnum2;
-	getinput(num1, num2);
+	num1 = getinput(num1, num2);
 	cout << " Before Swap" << setw(5) << num1 << setw(5) << num2 << setw(5) << endl;
 	swapTwoValues(num1, num2);
 	cout << " After Swap" << setw(5) << num1 << setw(5) << num2 << setw(5) << endl;
@@ -24,4 +26,16 @@ int main()
 
 // ******************************
 // Implement all your functions here
+int getinput(int n1, int n2)
+{
+	using namespace std;
+	cout << "Enter numbers: ";
+	cin >> n1 >> n2;
+
+	return 0;
+}
+int swapTwoValues(float n1, float n2)
+{
+	return 0;
+}
 // ******************************
